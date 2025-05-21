8cf5585331d9b49c7ee2bb8f6edddd4cabc3cc2d
ms-hamid <hamid.albase@gmail.com>
2025-05-22 06:38:48 +0700

    Enhance models with relationships and fillable attributes for Banner, Blog, Category, Comment, Gallery, Product, and User; implement polymorphic relationships in Comment and establish ownership tracking.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'category_name',
+        'category_type',
+    ];
+
+    protected $casts = [
+        'category_type' => 'enum',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
+
+    public function galleries()
+    {
+        return $this->hasMany(Gallery::class, 'category_id');
+    }
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class, 'category_id');
+    }
+
+
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'comment',
+        'user_id',
+        'target_id',
+        'target_type',
+        'parent_id',
+    ];
+
+    protected $casts = [
+        'target_type' => 'enum',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the parent target model (morphable relationship).
+     *
+     * This method defines a polymorphic relationship, allowing the comment
+     * to belong to different types of models (e.g., Post, Video, etc.).
+     */
+    public function target()
+    {
+        return $this->morphTo();
+    }
+    
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
