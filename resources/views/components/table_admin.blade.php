363e64ff0b51ec4d5f67ce3a022cf93a939d777f
Junior Dirgantara Betan <juniorajaspeed@gmail.com>
2025-05-25 00:51:09 +0700

    Update Edit Field Category

--- resources/views/components/table_admin.blade.php
+++ resources/views/components/table_admin.blade.php
@@ -84,18 +84,15 @@
                                                 @case('select')
                                                     <select name="{{ $field['name'] }}" id="{{ $field['name'] }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white">
                                                         @if(isset($field['options']) && is_array($field['options']))
-                                                        @foreach ($field['options'] as $key => $option)
-                                                            @if(is_array($option))
-                                                                <option value="{{ $option['value'] }}" 
-                                                                        {{ old($field['name'], $field['value'] ?? '') == $option['value'] ? 'selected' : '' }}>
-                                                                    {{ $option['label'] }}
+                                                            @foreach ($field['options'] as $key => $option)
+                                                                @php
+                                                                    $selectedValue = old($field['name'], $row->{$field['name']} ?? '');
+                                                                    $optionValue = is_array($option) ? $option['value'] : $key;
+                                                                    $optionLabel = is_array($option) ? $option['label'] : $option;
+                                                                @endphp
+                                                                <option value="{{ $optionValue }}" {{ $selectedValue == $optionValue ? 'selected' : '' }}>
+                                                                    {{ $optionLabel }}
                                                                 </option>
-                                                            @else
-                                                                <option value="{{ $key }}" 
-                                                                        {{ old($field['name'], $field['value'] ?? '') == $key ? 'selected' : '' }}>
-                                                                        {{ $option }}
-                                                                    </option>
-                                                                @endif
                                                             @endforeach
                                                         @endif
                                                     </select>
