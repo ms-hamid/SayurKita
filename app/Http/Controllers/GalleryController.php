3936f3f0f9c2d6d95e5719cb6250ca72b83eaaae
muhammad ilham <muhammadillhamm23@gmail.com>
2025-06-19 00:13:14 +0700

    pengunjung blog, gallery dan list blog

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -9,6 +10,12 @@ class GalleryController extends Controller
     //
     public function gallery()
     {
-        return view('pages.gallery');
+        // Ambil semua data galeri
+        $vegetables = Gallery::orderBy('created_at', 'desc')->get();
+
+        // Ambil kategori unik
+        $categories = $vegetables->pluck('category.category_name')->unique();
+
+        return view('pages.gallery', compact('vegetables', 'categories'));
     }
 }
--- app/Http/Controllers/ListBlogController.php
+++ app/Http/Controllers/ListBlogController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class ListBlogController extends Controller
 {
     public function list_blog()
     {
-        return view('pages.list_blog');
+        $blogs = Blog::with('category')->latest()->get();
+
+    // Ambil kategori unik dari relasi
+    $categories = $blogs->pluck('category.category_name')->filter()->unique();
+
+    return view('pages.list_blog', compact('blogs', 'categories')); 
     }
 }
