a251397dec274b6bd49e32b285e9c313a4866636
ms-hamid <hamid.albase@gmail.com>
2025-06-21 23:20:38 +0700

    fix: Standardize asset paths in footer and gallery templates

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,18 +1,3 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{asset('css/output.css')}}" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-  <!-- CSS for carousel/flickity-->
-  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
-  <link rel="stylesheet" href="https://unpkg.com/flickity-fade@2/flickity-fade.css">
-  
-  <!-- CSS for modal/flowbite -->
-  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
-</head>
-
 <footer class="bg-cp-black w-full relative overflow-hidden mt-20">
     <div class="container max-w-[1130px] mx-auto flex flex-wrap gap-y-4 items-center justify-between pt-[100px] pb-[220px] relative z-10">
       <div class="flex flex-col gap-10">
--- resources/views/layouts/gallery.blade.php
+++ resources/views/layouts/gallery.blade.php
@@ -3,7 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('styles/output.css') }}" rel="stylesheet">
+  <link href="{{asset('css/output.css')}}" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+  <!-- CSS for carousel/flickity-->
+  <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/flickity-fade@2/flickity-fade.css">
+  
+  <!-- CSS for modal/flowbite -->
+  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" /> -->
   <title>@yield('title', 'Default Title')</title>
 
   <!-- Fonts -->
